feb204eda912d44d03aeb19db67fdfc1cd37bab9
m@rtlin2cz <martlin2cz@gmail.com>
2017-04-30 20:00:34 +0200

    Updated libgen-cli
     - added some captcha processors (unfortunatelly, only one of them is working :-( )

--- libgen-cli/download-pdf.js
+++ libgen-cli/download-pdf.js
@@ -6,7 +6,9 @@
 var request = require('request');
 var fs = require('fs');
 var cheerio = require('cheerio');
+var readline = require('readline');
 
+var request = request.defaults({jar: true});
 
 ///////////////////////////////////////////////////////////
 // process the input
@@ -20,7 +22,43 @@ var itemIndex = (process.argv.length > 2) ? process.argv[4] : 0;
 var sourceIndex = (process.argv.length > 2) ? process.argv[5] : 0;
 
 ///////////////////////////////////////////////////////////
-// handlers
+// utilities
+
+var readFromConsole = function(promptText, readHandler) {
+	console.log(promptText);
+	
+	var reader = readline.createInterface({
+		  input: process.stdin,
+		  output: process.stdout,
+		  terminal: false
+	});
+
+	reader.on('line', function(line){
+		var text = line.toString().trim();
+		readHandler(text);
+	});
+}
+
+var downloadFile = function(url, postData, outputFile) {
+	console.log("Downloading to file " + outputFile + " (post: " + JSON.stringify(postData) + ")\n " + url);
+
+	var file = fs.createWriteStream(outputFile);
+
+	if (postData) {
+//		request.post(url).form(postData).pipe(file);
+		request.post({url: url, form: postData}, function(err, resp, body) {
+			console.log(resp + ":"+  body);
+		});
+	} else {
+		request(url).pipe(file);
+	}
+
+}
+
+
+
+///////////////////////////////////////////////////////////
+// working with urls
 
 var constructQueryUrl = function (query) {
 	return "http://libgen.io/scimag/index.php" 
@@ -39,6 +77,9 @@ var normalizeItemUrl = function(url) {
 		return url;
 	}
 }
+
+///////////////////////////////////////////////////////////////////////////////
+// working with items
 
 var getIthItem = function(items, itemIndex, sourceIndex) {
 	if (itemIndex >= items.length) {
@@ -70,6 +111,107 @@ var printAllItems = function (items) {
 		}
 	}
 }
+
+///////////////////////////////////////////////////////////////////////////////
+// captcha processing
+
+var findCaptchaProcessor = function(item) {
+	switch (item.source) {
+		case "Libgen":
+			return processorLibgen;
+		case "Sci-Hub Moscow":
+			return processorSciHubMoscow;
+		case "Sci-Hub Ocean":
+			return processorSciHubOcean;
+		case "Sci-Hub Cyber":
+			return processorSciHubCyber;
+		case "BookSC":
+			return processorBookSC;
+		case "Torrents":
+
+		default:
+			console.error("Unsupported source " + item.source);
+			return null;	//identity
+	}
+}
+
+var processorLibgen = function(url, html, handler) {
+	var baseUrl = "";
+	var downloadLinkIndex = 1;
+
+	withDownloadLink(html, handler, baseUrl, downloadLinkIndex);
+}
+
+var processorSciHubMoscow = function(url, html, outputFile) {
+	processorSciHub("moscow", url, html, outputFile);
+}
+
+var processorSciHubOcean = function(url, html, outputFile) {
+	processorSciHub("ocean", url, html, outputFile);
+}
+
+var processorSciHubCyber = function(url, html, outputFile) {
+	processorSciHub("cyber", url, html, outputFile);
+}
+
+var processorBookSC = function(url, html, outputFile) {
+	var baseUrl = "";
+	var downloadLinkSelector = "a.ddownload.color2.dnthandler";
+
+	withDownloadLink(html, outputFile, baseUrl, downloadLinkSelector);
+}
+
+
+
+var processorSciHub = function(subdomain, url, html, outputFile) {
+	var baseUrl = "http://" + subdomain + ".sci-hub.bz";
+	var postUrl = url;
+	var captchaImageSelector = "#captcha";
+	var captchaInputName = "captcha_code";
+
+	withCaptchaImageHandler(html, outputFile, baseUrl, postUrl, captchaImageSelector, captchaInputName);
+}
+
+
+var withDownloadLink = function(html, outputFile, baseUrl, downloadLinkSelectorOrIndex) {
+	var $ = cheerio.load(html);
+
+	var $download;
+	if (isNaN(downloadLinkSelectorOrIndex)) {
+		$download = $(downloadLinkSelectorOrIndex);
+	} else {
+		$download	= $("a").eq(downloadLinkSelectorOrIndex);
+	}
+
+	var href = $download.attr("href");
+	var url = baseUrl + href;
+
+	downloadFile(url, null, outputFile);
+}
+
+
+var withCaptchaImageHandler = function(html, outputFile, baseUrl, postUrl, captchaImageSelector, captchaInputName) {
+	var $ = cheerio.load(html);
+	var $captcha = $(captchaImageSelector);
+	var src = $captcha.attr("src");
+	var url = baseUrl + src;
+
+	console.log("Downloading captcha image " + url);
+	var stream = fs.createWriteStream('/tmp/captcha');
+	request(url).pipe(stream);
+	//TODO open
+	
+	var readHandler = function(text) {
+			var postData = {};
+			postData[captchaInputName] = text;
+			downloadFile(postUrl, postData, outputFile);
+	}
+	readFromConsole("Enter captcha code", readHandler);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// parsing
+
 
 var processHTML = function(html) {
 	var $ = cheerio.load(html);
@@ -110,11 +252,27 @@ var processHTML = function(html) {
 	return items;
 }
 
+///////////////////////////////////////////////////////////////////////////////
+// download handlers
 var processItem = function(item, outputFile) {
-	console.log("Downloading '" + item.title + "' by '" + item.author + "' from " + item.source + "\n" + item.url);
+	console.log("Downloading '" + item.title + "' by '" + item.author + "' from " + item.source + "\n " + item.url);
 	
-	var stream = fs.createWriteStream(outputFile);
+	var captchaHandler =  function(error, response, body) {
+		if (error) {
+			console.error("Error #2: " + error);
+			return;
+		} else {
+//		console.log(body);			//XXX testing
+			var process = findCaptchaProcessor(item);
+			process(item.url, body, outputFile);
+		}
+	}
+	/*
+			var stream = fs.createWriteStream('bio');	//XXX testing
 	request(item.url).pipe(stream);
+	*/
+
+	request.get(item.url, captchaHandler);
 }
 
 var doTheQuery = function(query, outputFile, itemIndex, sourceIndex) {
@@ -129,6 +287,9 @@ var doTheQuery = function(query, outputFile, itemIndex, sourceIndex) {
 			console.log("Found " + items.length + " items");
 			if (outputFile) {
 				var item = getIthItem(items, itemIndex, sourceIndex);
+				if (!item) {
+					return;
+				}
 				processItem(item, outputFile);
 			} else {
 				printAllItems(items);
