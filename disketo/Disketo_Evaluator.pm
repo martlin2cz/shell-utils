076066e6d46114642df209a3b6918ec3a231e1a1
m@rtlin2cz <martlin2cz@gmail.com>
2019-02-13 22:50:08 +0100

    Completed parser with validator (file->program)
     - yea, also tested
     - now up to print/eval phase

--- disketo/Disketo_Evaluator.pm
+++ disketo/Disketo_Evaluator.pm
@@ -18,21 +18,23 @@ sub run($@) {
 	my $script = shift @_;
 	my @params = @_;
 
-	my $program_ref = parse($script);
-	evaluate($program_ref, \@params);
+	my $parsed_ref = parse($script);
+	my $program_ref = validate($parsed_ref, \@params);
+	#TODO if --dry-run print else
+	#TODO if no such params, print usage
+	evaluate($program_ref);
 }
 
 #######################################
 
-sub parse($) {
-	my ($script_file) = (@_);
+sub parse($$) {
+	my ($script_file, $program_args_ref) = (@_);
 	
 	my $content = load_file($script_file);
-	my $simply_parsed_ref = parse_simply($content);
-	
-	#TODO 
+	my $parsed_ref = parse_content($content);
+	my ($program_ref, $program_args_mod_ref) = validate($parsed_ref, $program_args_ref);
 
-	return $simply_parsed_ref;
+	return ($program_ref, $program_args_mod_ref);
 }
 
 sub load_file($) {
@@ -51,17 +53,27 @@ sub load_file($) {
 
 #######################################
 
-sub parse_simply($) {
+sub parse_content($) {
 	my ($content) = @_;
 
 	my @tokens = tokenize($content);
 	my @result = ([]);
 	for my $token (@tokens) {
-		if ($token =~ /^\n$/) {
-			push @result, [];
-		} else {
+		if ($token =~ /^#/) {
+			next;
+		}
+		elsif ($token eq "\n") {
+			if (scalar @{ @result[-1] } > 0) { #if previous line wasn't empty
+				push @result, [];
+			}
+		} 
+		else {
 			push @{ @result[-1] }, $token;
 		}
+	}
+
+	if (scalar @{ @result[-1] } == 0) { 
+		pop @result;
 	}
 
 	return \@result;
@@ -77,18 +89,56 @@ sub tokenize($) {
 		(\" [^\"]* \") | 
 		(?# regular text)
 		( [\w]+ ) | 
+		(?# the $$ marker)
+		( \$\$ ) | 
 		(?# pass newlines too)
-		( \n ) /gx;
+		( \n ) |
+		(?# comments)
+		(\# [^\n]* \n) /gx;
 		
-	#TODO if  "sub", "{...}" then replace with "sub {...}"
 	my @filtered = grep /(.+)|(\n)/, @parts;
+	my @collapsed = collapse_subs(@filtered);
 
-	return @filtered;
+	return @collapsed;
+}
+
+sub collapse_subs(@) {
+	my @tokens = @_;
+	my @result = ();
+
+	for (my $i = 0; $i < scalar @tokens; $i++) {
+		my $token = @tokens[$i];
+		if ($token eq "sub") {
+			$i++;
+			$token = @tokens[$i];
+			$token = "sub " . $token;
+		}
+		push @result, $token;
+	}
+
+	return @result;
 }
 
 #######################################
 
-sub validate() {
+sub validate($$) {
+	my ($parsed_ref, $program_args_ref) = @_;
+
+	my $functions_ref = functions_table();
+	my @program = ();
+
+	for my $statement_ref (@{ $parsed_ref }) {
+		my ($statement_mod_ref, $fnname, $function_ref) = validate_function($statement_ref, $functions_ref);
+		
+		my $resolved_args_ref;
+		($resolved_args_ref, $program_args_ref) = validate_params($statement_mod_ref, $program_args_ref, $fnname, $function_ref);
+
+		my %instruction = ( "function" => $function_ref, "arguments" => $resolved_args_ref );
+		push @program, \%instruction;
+	}
+
+	return (\@program, $program_args_ref);
+
 	#TODO for each line:
 	#TODO  check whether the function exists
 	#TODO  check whether requires loaded roots/stats
@@ -97,13 +147,99 @@ sub validate() {
 	#TODO  extract @ARGV values, put into instead of $$s
 }
 
+sub validate_function($$) {
+	my ($statement_ref, $functions_ref) = @_;
+
+	my @statement = @{ $statement_ref };
+	my $fnname = shift @statement;
+
+	my $function_ref = %{ $functions_ref }{$fnname};
+	if (!$function_ref) {
+		die("Unknown command $fnname");
+	}
+
+	return (\@statement, $fnname, $function_ref);
+}
+
+sub validate_params($$$$) {
+	my ($statement_ref, $program_args_ref, $fnname, $function_ref) = @_;
+	my @program_args = @{ $program_args_ref };
+	my %function = %{ $function_ref };
+	my @params = @{ $function{"params"} };
+	my @arguments = @{ $statement_ref };
+
+	if ((scalar @params) ne (scalar @arguments)) {
+		die("$fnname expects " . (scalar @params) . " params (" . join(", ", @params) . "), "
+				. "given " . (scalar @arguments) . "(". join(", ", @arguments) . ")");
+	}
+
+	my @resolved_args = ();
+	for my $arg (@arguments) {
+		if ($arg eq '$$') {
+			if (scalar @program_args == 0) {
+				die("No such arguments at $fnname");
+			}
+			$arg = shift @program_args;
+			push @resolved_args, $arg;
+		}
+		elsif ($arg =~ /^sub ?\{/) {
+			my $fn = eval($arg);
+			push @resolved_args, $fn;
+		}
+		else {
+			push @resolved_args, $arg;
+		}
+	}
+
+	return (\@resolved_args, \@program_args);
+}
+
+#######################################
+
+
 sub print() {
 	#TODO for each line:
 	#TODO  print it somehow
 }
 
-sub evalueate() {
+sub evaluate() {
 	#TODO for each line:
 	#TODO  evaluate it somehow
 }
 
+#######################################
+
+sub functions_table() {
+	my %table = (
+		"list_all_directories" => { "name" => "list_all_directories", 
+			"requires_list" => 0, "requires_stats" => 0, "params" => [""]},
+		"load_stats" => { "name" => "load_stats", 
+			"requires_list" => 1, "requires_stats" => 0, "params" => [""]},
+		"filter_directories_custom" => { "name" => "filter_directories_custom", 
+			"requires_list" => 1, "requires_stats" => 0, "params" => ["predicate"]},
+		"filter_directories_by_pattern" => { "name" => "filter_directories_by_pattern", 
+			"requires_list" => 1, "requires_stats" => 0, "params" => ["pattern"]},
+		"filter_directories_by_files_pattern" => { "name" => "filter_directories_by_files_pattern", 
+			"requires_list" => 1, "requires_stats" => 0, "params" => ["pattern", "threshold"]},
+		"filter_directories_matching" => { "name" => "filter_directories_matching", 
+			"requires_list" => 1, "requires_stats" => 0, "params" => ["matcher"]},
+		"filter_directories_of_same_name" => { "name" => "filter_directories_of_same_name", 
+			"requires_list" => 1, "requires_stats" => 0, "params" => []},
+		"filter_directories_with_common_files" => { "name" => "filter_directories_with_common_files", 
+			"requires_list" => 1, "requires_stats" => 0, "params" => ["min_count", "files_matcher"]},
+		"filter_directories_with_common_file_names" => { "name" => "filter_directories_with_common_file_names", 
+			"requires_list" => 1, "requires_stats" => 0, "params" => ["min_coun"]},
+		"filter_directories_with_common_file_names_with_size" => { "name" => "filter_directories_with_common_file_names_with_size", 
+			"requires_list" => 1, "requires_stats" => 1, "params" => ["min_count"]},
+		"print_directories_simply" => { "name" => "print_directories_simply", 
+			"requires_list" => 1, "requires_stats" => 0, "params" => []},
+		"print_directories" => { "name" => "print_directories", 
+			"requires_list" => 1, "requires_stats" => 0, "params" => ["printer"]},
+		"print_files" => { "name" => "print_files", 
+			"requires_list" => 1, "requires_stats" => 0, "params" => ["printer"]}
+		#		"" => { "name" => "", 
+		#	"requires_list" => 1, "requires_stats" => 1, "params" => [""]},
+	);
+
+	return \%table;
+}
--- disketo/test_Disketo_Evaluator.pl
+++ disketo/test_Disketo_Evaluator.pl
@@ -17,14 +17,14 @@ my $content1 = Disketo_Evaluator::load_file($file1);
 print ">>> $content1 <<<\n";
 
 #######################################
-Disketo_Utils::logit("parse_simple");
+Disketo_Utils::logit("parse_content");
 
-my $content2a = "foo\tbar\tbaz\nkarel\t42\n";
-my $parsed2a_ref = Disketo_Evaluator::parse_simply($content2a);
+my $content2a = "foo bar baz karel 42\n";
+my $parsed2a_ref = Disketo_Evaluator::parse_content($content2a);
 print Dumper($parsed2a_ref);
 
-my $content2b = "foo_bar_baz\t\"99\tluftbalons\"\t\"333\"\nlorem\t42\tsub { \n\t\"42\";\n }\n";
-my $parsed2b_ref = Disketo_Evaluator::parse_simply($content2b);
+my $content2b = "foo_bar_baz \"99 luftbalons\" \t \"333\"\nlorem\t42\tsub { \n\t\"42\";\n } \$\$ ## test\n";
+my $parsed2b_ref = Disketo_Evaluator::parse_content($content2b);
 print Dumper($parsed2b_ref);
 
 
@@ -35,4 +35,49 @@ my @tokens3a_ref = Disketo_Evaluator::tokenize($content2a);
 print Dumper(\@tokens3a_ref);
 my @tokens3b_ref = Disketo_Evaluator::tokenize($content2b);
 print Dumper(\@tokens3b_ref);
+
+#######################################
+Disketo_Utils::logit("collapse_subs");
+
+my @filtered8a = ("foo", "sub", "{ 42; }", "bar", "sub{return 1;}", "baz");
+my @collapsed8a = Disketo_Evaluator::collapse_subs(@filtered8a);
+print Dumper(\@collapsed8a);
+
+
+
+#######################################
+Disketo_Utils::logit("functions_table");
+my $table4_ref = Disketo_Evaluator::functions_table();
+print Dumper($table4_ref);
+
+#######################################
+Disketo_Utils::logit("validate_function");
+my @statement5a = ("filter_directories_matching", "42", "karel");
+my ($statement5a_mod_ref, $fnname5a, $function5a_ref) = Disketo_Evaluator::validate_function(\@statement5a, $table4_ref);
+print Dumper($statement5a_mod_ref, $fnname5a, $function5a_ref);
+
+my @statement5b = ("foo_bar_baz", "lorem", "ipsum");
+### Disketo_Evaluator::validate_function(\@statement5b, $table4_ref);
+
+#######################################
+Disketo_Utils::logit("validate_params");
+my @statement6a_mod = ("42", "karel", "\$\$", "\"boo\"", "sub { 99; }");
+my @program_args_6a = ("foo", "bar", "baz");
+my %function6 = ( "method" => &Disketo_Utils::logit, "params" => ["first", "second", "third", "fourth", "fifth"] );
+
+my ($resolved_args6, $program_args_mod_6) = Disketo_Evaluator::validate_params(\@statement6a_mod, \@program_args_6a, $fnname5a, \%function6);
+print Dumper($resolved_args6, $program_args_mod_6);
+
+my @statement6b_mod = ("foo", "bar", "baz", "42");
+### Disketo_Evaluator::validate_params(\@statement6b_mod, \@program_args_6a, $fnname5a, \%function6);
+
+my @statement6b_mod = ("\$\$", "\$\$", "\$\$", "\$\$", "\$\$");
+### Disketo_Evaluator::validate_params(\@statement6b_mod, \@program_args_6a, $fnname5a, \%function6);
+
+#######################################
+Disketo_Utils::logit("parse");
+my $script7a = "test/scripts/simple.ds";
+my @program_args_7a = ("42", "99", "foo", "bar");
+my ($program7a_ref, $program_args_mod7a_ref) = Disketo_Evaluator::parse($script7a, \@program_args_7a);
+print Dumper($program7a_ref, $program_args_mod7a_ref);
 
