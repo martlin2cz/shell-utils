fc677cf018262aa27597e93d63318c66ebc0dd88
m@rtlin2cz <martlin2cz@gmail.com>
2019-02-17 20:59:14 +0100

    Cleanup, added two more scripts
     - yea
     - also added --list-functions option to run-script

--- disketo/run-disketo-script.pl
+++ disketo/run-disketo-script.pl
@@ -8,16 +8,37 @@ use Disketo_Evaluator;
 
 #######################################
 my $dry_run = 0;
+
+if ((scalar @ARGV > 0) and ((@ARGV[0] eq "--list") or (@ARGV[0] eq "--list-functions"))) {
+  list_functions();
+  die("That's all folks!");
+}
+
+
 if ((scalar @ARGV > 0) and ((@ARGV[0] eq "--dry") or (@ARGV[0] eq "--dry-run"))) {
   $dry_run = 1;
   shift @ARGV;
 }
 
-Disketo_Utils::usage(\@ARGV, "[--dry|--dry-run] <SCRIPT> <SCRIPT PARAMS...>");
+Disketo_Utils::usage(\@ARGV, "[--dry|--dry-run] <SCRIPT> <SCRIPT PARAMS...>\n" 
+	. "Use --list or --list-functions to list supported functions");
 
 my $script = shift @ARGV;
 my @args = @ARGV;
 
 Disketo_Evaluator::run($dry_run, $script, \@args);
 
+#######################################
+
+sub list_functions() {
+	my $table_ref = Disketo_Evaluator::functions_table();
+	my %table = %{ $table_ref };
+
+	for my $fnname (sort keys %table) {
+		my $function_ref = $table{$fnname};
+		my $params_ref = $function_ref->{"params"};
+	
+		print STDERR "$fnname\t" . join ("\t", @{ $params_ref }) . "\n";
+	}
+}
 
