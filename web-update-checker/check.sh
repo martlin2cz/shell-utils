21e94e677c1e71a5b0bbf0a84f70583d8fcf49fb
martlin2cz2 <martlin@seznam.cz>
2019-10-08 20:53:37 +0200

    Created file
     - with header
     - and simple loop

---
+++ web-update-checker/check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+## web update checker
+## m@rtlin, 8.10.2019
+## 
+## in given interval watches (crawls) the given URL 
+## and when starts to contain specified text, does something
+##
+
+while [ true ] ; do
+	#TODO
+	echo hi
+	sleep 1
+done
