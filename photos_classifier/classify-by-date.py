4da47ce481c1413e5924c083bd826097d0927dd8
martlin@hpmini <martlin (at) seznam.cz>
2022-12-30 23:03:18 +0100

    photos_classifier: Completed the CLI

--- photos_classifier/classify-by-date.py
+++ photos_classifier/classify-by-date.py
@@ -11,6 +11,7 @@ import datetime
 import os
 import shutil
 import logging
+import argparse
 
 ###############################################################################
 
@@ -57,6 +58,8 @@ def date_of_photo(photo_file):
        
 def list_files(directory, recurse):
     """ Lists all the files in the given directory, possibly recurring """
+
+    #TODO ensure existence
 
     result = []
     for (dirpath, dirs, files) in os.walk(directory):
@@ -113,11 +116,20 @@ def print_date_histogram(groups, quora = None):
         quora_bar_str = HISTO_CHAR * quora
         print("%9s : %s" % ("quora", quora_bar_str))
     
-    for date in sorted(groups.keys()):
+    #TODO print all dates from first to last, if specified
+    for date in groups.keys():
         date_str = date.strftime(HISTO_DATE_FORMAT)
         files_count = len(groups[date])
         bar_str = HISTO_CHAR * files_count
         print("%9s : %s" % (date_str, bar_str))
+
+def print_files_by_date(groups):
+    """ Prints just the date->list of files """
+
+    for date in sorted(groups.keys()):
+        date_str = date.strftime(HISTO_DATE_FORMAT)
+        files = groups[date]
+        print(date_str + " -> " + str(files))
 
 def copy_or_move(groups, quora, action, target_owner):
     """ Copies or moves the photos in groups excessing the quora into the specified target dir """
@@ -151,23 +163,93 @@ def copy_or_move_file(photo_file, action, group_dir):
     else:
         raise Exception("Either copy or move is allowed")
 
-def run(directories, recurse):
+def _run(directories, recurse):
+    """ Does some action (method for debug/testing purposes) """
+
     groups = load_and_group(directories, recurse)
-    print_date_histogram(groups, 2)
+    print("uncomment me")
+    #print_date_histogram(groups, 2)
     #copy_or_move(groups, 2, "copy", "/tmp/photos-1")
+
+def doit(directory, recurse, action, quora = None, destination = None):
+    """ Does the actual action with the given directory (possibly recursivelly), with the optional quora and destination) """
+    groups = load_and_group(directory, recurse)
+
+    if action == "histo":
+        print_date_histogram(groups, quora)
+
+    if action == "list":
+        print_files_by_date(groups)
+
+    if action == "copy" or action == "move":
+        copy_or_move(groups, quora, action, destination)
+
+def check_args(parsed_args):
+    """ Validates the provided args """
+
+    if not os.path.isdir(parsed_args.directory):
+        raise ValueError(f"The ${parsed_args.directory} is not a directory")
+
+    if parsed_args.destination and not os.path.isdir(parsed_args.destination):
+        raise ValueError(f"The ${parsed_args.destionation} is not a directory")
+
+
+def configure_logging(verbose, debug):
+    """ Sets the logger configuration based on the command line flags """
+
+    if not (verbose or debug):
+        return
+    
+    if verbose:
+        new_level = logging.INFO
+
+    if debug:
+        new_level = logging.DEBUG
+
+    logging.basicConfig(level = logging.INFO, format="%(asctime)s %(levelname)8s %(name)s %(message)s")
+    LOGGER.setLevel(new_level)
 
 ###############################################################################
 
+parser = argparse.ArgumentParser(
+        description = "Classifies (by coping, moving, or just outputting to console) photos by date (day)")
+
+parser.add_argument("-a", "--action", action = "store",
+        choices = ["list", "histo", "copy", "move"], 
+        default = "histo",
+        help = "What to do with the result. Just output the date->files mapping, show a histogram (date->number of files) (DEFAULT) or copy or move the files into folder for each day?")
+
+parser.add_argument("-r", "--recursive", action = "store_true",
+        help = "If set, will look for the photo files in the DIRECTORY recursivelly")
+
+parser.add_argument("-q", "--quora", action = "store", type = int,
+        help = "Specifies what amount of photos per day starts to be interresting (smaller amount ignores the day by -a=copy and -a=move)")
+
+parser.add_argument("-d", "--destination", action = "store",
+        help = "When -a=copy or -a=move set, specifies where to copy/move the files to (their owning group dirs)")
+
+parser.add_argument("-v", "--verbose", action = "store_true",
+        help = "Enables verbose output")
+
+parser.add_argument("-D", "--debug", action = "store_true",
+        help = "Enables full debug output")
+
+parser.add_argument("directory", metavar = "DIRECTORY",
+        help = "The directory to scan for the photo files (either --recursive or not)")
+
 if __name__ == "__main__":
+    parsed = parser.parse_args()
+    check_args(parsed)
+    configure_logging(parsed.verbose, parsed.debug)
+    doit(parsed.directory, parsed.recursive, parsed.action, parsed.quora, parsed.destination)
+
 #    print(date_of_photo("testing-images/detaily-o-zasilce.jpg"))
 #    print(date_of_photo("testing-images/kus-qeerka.jpg"))
 #    print(list_files("testing-images", False))
 #    print(list_files(".", True))
 #    print(load_and_group("testing-images", True))
-     logging.basicConfig(level = logging.INFO)
-     LOGGER.setLevel(logging.DEBUG)
-     #print(LOGGER)
-     print(run("testing-images", True))
-
-
+#    logging.basicConfig(level = logging.INFO, format="%(asctime)s %(levelname)8s %(name)s %(message)s")
+#    LOGGER.setLevel(logging.DEBUG)
+#    print(run("testing-images", True))
+#    print(parser.parse_args(["-v", "--debug", "--quora", "42", "foobar"]))
  
